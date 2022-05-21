6df3a03d2fff0e9d2d2f313f1acf3fc38e8271f5
Nnamdi <nnamdi.victor.chibueze@gmail.com>
2022-05-21 22:19:14 +0100

    Add files via upload

---
+++ hackerrank2darray.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+func diagonalDifference(arr [][]int32) int32 {
+	// Write your code here
+	var a []int32
+	var b []int32
+	a = append(a, arr[0][0], arr[1][1], arr[2][2])
+	b = append(b, arr[0][2], arr[1][1], arr[2][0])
+	fmt.Println(a)
+	fmt.Println(b)
+	sumA := int32(0)
+	sumB := int32(0)
+	var c int32
+	for _, v := range a {
+		sumA += v
+	}
+	for _, v := range b {
+		sumB += v
+	}
+	if sumA > sumB {
+		c = int32(sumA - sumB)
+	} else if sumB > sumA {
+		c = int32(sumB - sumA)
+	}
+	return c
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	var arr [][]int32
+	for i := 0; i < int(n); i++ {
+		arrRowTemp := strings.Split(strings.TrimRight(readLine(reader), " \t\r\n"), " ")
+
+		var arrRow []int32
+		for _, arrRowItem := range arrRowTemp {
+			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+			checkError(err)
+			arrItem := int32(arrItemTemp)
+			arrRow = append(arrRow, arrItem)
+		}
+
+		if len(arrRow) != int(n) {
+			panic("Bad input")
+		}
+
+		arr = append(arr, arrRow)
+	}
+
+	result := diagonalDifference(arr)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
